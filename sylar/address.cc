2ceaa6c93f92be501cbe8ac569be66de698e701d
ynwad <qingchenchn@gmail.com>
2024-01-24 20:28:24 +0800

    add address module

--- sylar/address.cc
+++ sylar/address.cc
@@ -1,6 +1,6 @@
 #include "address.h"
 #include <sstream>
-// #include <netdb.h>
+#include <netdb.h>
 // #include <ifaddrs.h>
 // #include <stddef.h>
 #include "log.h"
@@ -65,6 +65,16 @@ bool Address::Lookup(std::vector<Address::ptr>& result, const std::string& host,
     if(node.empty()) {
         node = host;
     }
+    // 处理名字到地址以及服务到端口这两种转换，返回的是一个addrinfo的结构（列表）指针
+    // 函数原型
+    // int getaddrinfo( const char *hostname, const char *service, const struct addrinfo *hints, struct addrinfo **result );
+    // hostname:一个主机名或者地址串(IPv4的点分十进制串或者IPv6的16进制串)
+    // service：服务名可以是十进制的端口号，也可以是已定义的服务名称，如ftp、http等
+    // hints：可以是一个空指针，也可以是一个指向某个addrinfo结构体的指针，
+    //        调用者在这个结构中填入关于期望返回的信息类型的暗示。
+    //        举例来说：如果指定的服务既支持TCP也支持UDP，那么调用者可以把hints结构中的ai_socktype
+    //        成员设置成SOCK_DGRAM使得返回的仅仅是适用于数据报套接口的信息。
+    // result：本函数通过result指针参数返回一个指向addrinfo结构体链表的指针。
     int error = getaddrinfo(node.c_str(), service, &hints, &results);
     if(error) {
         SYLAR_LOG_DEBUG(g_logger) << "Address::Lookup getaddress(" << host << ", "
